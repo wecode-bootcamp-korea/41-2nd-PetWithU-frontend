492bcc8003433a765e6673bea2a1409e962b43a0
baegyungmin <bjy6439@naver.com>
2023-01-31 19:21:04 +0900

    "Add : NAV기능 및 레이아웃 구현 완료 "

---
+++ src/components/Search/SearchList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as S from './Search.styles';
+
+const SearchList = ({ searchItemList }) => {
+  return (
+    <S.SearchListContainer>
+      <ul>
+        {searchItemList.map(({ id, name, thumbnail, price }) => {
+          return (
+            <S.SearchItemList key={id}>
+              <S.SearchItemImg src={thumbnail} />
+              <S.ItemDec>
+                <S.ItemText primary>{name}</S.ItemText>
+                <S.ItemText>{Math.floor(price)}원</S.ItemText>
+              </S.ItemDec>
+            </S.SearchItemList>
+          );
+        })}
+      </ul>
+    </S.SearchListContainer>
+  );
+};
+
+export default SearchList;
