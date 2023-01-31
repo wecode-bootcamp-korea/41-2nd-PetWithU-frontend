88dc0c9e86ad5afc407866642ec30808485f6bbe
baegyungmin <bjy6439@naver.com>
2023-01-31 19:21:04 +0900

    "Add : NAV기능 및 레이아웃 구현 완료 "

--- src/components/Nav/Nav.styles.js
+++ src/components/Nav/Nav.styles.js
@@ -1,25 +1,30 @@
 import styled from 'styled-components';
+import theme from '../../styles/theme';
 
 export const Nav = styled.div`
+  position: sticky;
+  top: 0px;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100vw;
-  height: 15vh;
+  height: 100px;
   background-color: white;
-  border-bottom: 3px solid ${props => props.theme.pointColor};
+  border-bottom: 1px solid ${props => props.theme.pointColor};
+  z-index: 10;
 `;
 
 export const NavBox = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  width: 70%;
+  width: 100%;
   height: 100px;
 `;
 
 export const Logo = styled.p`
   font-size: 30px;
+  cursor: pointer;
 `;
 
 export const NavMenu = styled.button`
@@ -30,14 +35,25 @@ export const NavMenu = styled.button`
 `;
 
 export const InputBox = styled.input`
-  width: 30%;
-  height: 50%;
+  width: 300px;
+  height: 40px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: ${theme.radius};
+  padding: 15px;
+`;
+
+export const MyBtnList = styled.div`
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const MyBtn = styled.button`
   background-color: white;
   border: none;
-  font-size: 20px;
+  margin: 0px 10px 0px 10px;
+  font-size: ${props => (props.primary ? '20px' : '30px')};
   cursor: pointer;
 `;
 
@@ -50,4 +66,27 @@ export const AddFeedBtn = styled.button`
   color: white;
   font-size: 20px;
   cursor: pointer;
+`;
+
+export const WriteList = styled.ul`
+  position: absolute;
+  top: 80px;
+  right: 50px;
+  width: 200px;
+  height: 100px;
+  border: 1px solid black;
+  border-radius: ${theme.radius};
+  background-color: white;
+  overflow: hidden;
+`;
+export const WriteBtn = styled.li`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  padding: 20px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${theme.pointColor};
+    color: white;
+  }
 `;
