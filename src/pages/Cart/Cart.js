20f6c23f4e49c08b26d677429dc4bd384eea1774
kwonnahyun <skgusskgus2@naver.com>
2023-02-06 13:23:32 +0900

    cart페이지 레이아웃 & 기능 추가

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Ci from './CartStyle';
+import CartRight from './CartRight';
+import CartLeft from './CartLeft';
 
 function Cart() {
-  return <div>Cart</div>;
+  const [item, setItem] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  useEffect(() => {
+    setItem(CART_ITEM);
+  }, []);
+  console.log(item);
+  return (
+    <Ci.CartContainer>
+      <CartLeft
+        item={item}
+        setItem={setItem}
+        totalPrice={totalPrice}
+        setTotalPrice={setTotalPrice}
+      />
+      <CartRight totalPrice={totalPrice} />
+    </Ci.CartContainer>
+  );
 }
 
 export default Cart;
+const CART_ITEM = [
+  {
+    id: 1,
+    name: '고양이 사료',
+    url: 'https://images.unsplash.com/photo-1501820488136-72669149e0d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    price: 10000,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: '강아지 사료',
+    url: 'https://images.unsplash.com/photo-1647616350787-6428e907a7fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80',
+    price: 20000,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: '애완용품',
+    url: 'https://images.unsplash.com/photo-1527150602-a98f7a6f2746?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+    price: 30000,
+    quantity: 1,
+  },
+];
---
+++ src/pages/Cart/CartLeft.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect } from 'react';
+import * as Ci from './CartStyle';
+import CartList from './CartList';
+import useCart from './hooks/useCart';
+
+export default function CartLeft({ item, setItem, totalPrice, setTotalPrice }) {
+  const [
+    isAllChecked,
+    allCheck,
+    handleCheckBox,
+    found,
+    checkList,
+    setCheckList,
+  ] = useCart(item);
+
+  //  아이템 수량 구하기
+  const handleQuantity = (type, id, quantity) => {
+    const found = item.filter(el => el.id === id)[0];
+    const idx = item.indexOf(found);
+    if (quantity === 0) {
+      return;
+    }
+    const itemBox = {
+      id: found.id,
+      name: found.name,
+      url: found.url,
+      price: found.price,
+      quantity: quantity,
+    };
+    if (type === 'plus') {
+      setItem([...item.slice(0, idx), itemBox, ...item.slice(idx + 1)]);
+    } else {
+      setItem([...item.slice(9, idx), itemBox, ...item.slice(idx + 1)]);
+    }
+  };
+
+  const handleRemove = id => {
+    setItem(item.filter(el => el.id !== id));
+    setCheckList(checkList.filter(check => check !== id));
+  };
+
+  // 전체 수량의 총 합계
+  useEffect(() => {
+    if (found) {
+      const filter = found.filter(item => item.length !== 0);
+
+      const sum = filter.map(item => item[0].price * item[0].quantity);
+      const reducer = (a, b) => a + b;
+
+      if (sum.length === 0) {
+        setTotalPrice(0);
+        return;
+      }
+      const itemTotal = sum.reduce(reducer);
+      setTotalPrice(itemTotal);
+    } else {
+      setTotalPrice(0);
+    }
+  }, [item, found]);
+  return (
+    <Ci.CartLeft>
+      <Ci.CartLeftTop>
+        <Ci.CartTotalSelect
+          type="checkbox"
+          id="select"
+          checked={isAllChecked}
+          onChange={e => {
+            allCheck(e.currentTarget.checked);
+          }}
+        />
+        <label htmlFor="select" className="selectLabel">
+          전체선택
+        </label>
+      </Ci.CartLeftTop>
+
+      <Ci.CartListContainer>
+        <Ci.CartList>
+          {item.map(e => {
+            return (
+              <CartList
+                item={e}
+                key={e.id}
+                setTotalPrice={setTotalPrice}
+                checkList={checkList}
+                handleCheckBox={handleCheckBox}
+                handleRemove={handleRemove}
+                found={found}
+                handleQuantity={handleQuantity}
+              />
+            );
+          })}
+        </Ci.CartList>
+      </Ci.CartListContainer>
+      <Ci.CartListTotal>
+        총금액<span>{totalPrice} 원</span>
+      </Ci.CartListTotal>
+    </Ci.CartLeft>
+  );
+}
