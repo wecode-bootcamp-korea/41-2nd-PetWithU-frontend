6619877293c47dfe0f9edce8d30767b025d6dc87
baegyungmin <bjy6439@naver.com>
2023-02-09 04:21:36 +0900

    Add : footer layout 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import * as F from './Footer.styles';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <F.FooterContainer>
+      <F.CompanyInfo>
+        <F.InfoList>
+          <li>
+            <F.Center>고객센터</F.Center>
+          </li>
+          <li>
+            <F.MiddleSpan>1600-0876</F.MiddleSpan>
+            <F.MiddleSpan>09:00 ~ 18:00</F.MiddleSpan>
+          </li>
+          <li>
+            <F.Small>평일: 전체 문의 상담 가능</F.Small>
+          </li>
+          <li>
+            <F.Small>
+              주말, 공휴일: 오늘의집 직접배송, 이사/시공/수리 문의 상담 가능
+            </F.Small>
+          </li>
+          <li>
+            <F.TalkBtn>카톡 상담(평일 09:00~18:00)</F.TalkBtn>
+          </li>
+          <li>
+            <F.TalkBtn>이메일 문의</F.TalkBtn>
+          </li>
+        </F.InfoList>
+      </F.CompanyInfo>
+      <F.CompanyBtnList>
+        <F.FirstList>
+          <li>
+            <F.Small>회사소개</F.Small>
+          </li>
+          <li>
+            <F.Small>채용정보</F.Small>
+          </li>
+          <li>
+            <F.Small>이용약관</F.Small>
+          </li>
+          <li>
+            <F.Small>개인정보</F.Small>
+          </li>
+          <li>
+            <F.Small>처리방침</F.Small>
+          </li>
+          <li>
+            <F.Small>공지사항</F.Small>
+          </li>
+        </F.FirstList>
+        <F.SecondList>
+          <li>
+            <F.Small>안전거래센터</F.Small>
+          </li>
+          <li>
+            <F.Small>입점신청</F.Small>
+          </li>
+          <li>
+            <F.Small>제휴/광고 문의</F.Small>
+          </li>
+          <li>
+            <F.Small>사업자 구매 회원</F.Small>
+          </li>
+          <li>
+            <F.Small>시공파트너 안내</F.Small>
+          </li>
+          <li>
+            <F.Small>상품광고 소개</F.Small>
+          </li>
+        </F.SecondList>
+      </F.CompanyBtnList>
+      <F.Img src="/images/123.png" />
+    </F.FooterContainer>
+  );
 }
 
 export default Footer;
---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+  height: 300px;
+  margin-top: 30px;
+  border-top: 1px solid black;
+  padding: 30px;
+`;
+
+export const CompanyInfo = styled.div``;
+
+export const InfoList = styled.ul``;
+
+export const Center = styled.p`
+  font-size: 18px;
+  margin-bottom: 15px;
+  font-weight: 700;
+`;
+
+export const Big = styled.p`
+  font-size: 16px;
+`;
+
+export const MiddleSpan = styled.span`
+  font-size: 14px;
+  margin-right: 5px;
+`;
+
+export const Small = styled.p`
+  font-size: 12px;
+  margin-bottom: 10px;
+`;
+
+export const CompanyBtnList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-left: 1px solid gray;
+  border-right: 1px solid gray;
+`;
+
+export const FirstList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 250px;
+  margin: 0px 20px 0px 40px;
+`;
+export const SecondList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 250px;
+  margin: 0px 40px 0px 20px;
+`;
+
+export const Img = styled.img`
+  width: 40%;
+`;
+
+export const TalkBtn = styled.button`
+  width: 200px;
+  height: 30px;
+  margin: 5px 0px;
+`;
