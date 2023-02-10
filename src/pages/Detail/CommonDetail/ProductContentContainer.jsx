d1cf06766308554ed6a439b5286d790b733ff16d
kwonnahyun <skgusskgus2@naver.com>
2023-02-10 09:56:42 +0900

    Modify 서버통신연결

--- src/pages/Detail/CommonDetail/ProductContentContainer.jsx
+++ src/pages/Detail/CommonDetail/ProductContentContainer.jsx
@@ -3,11 +3,11 @@ import ProductContentBottom from './ProductContentBottom';
 import ProductContentTop from './ProductContentTop';
 import * as P from './ProductDetailStyle';
 
-export default function ProductContentContainer({ detailContent }) {
+export default function ProductContentContainer({ imgData, setImgData }) {
   return (
     <P.ProductContentContainer>
-      <ProductContentTop detailContent={detailContent} />
-      <ProductContentBottom detailContent={detailContent} />
+      <ProductContentTop imgData={imgData} setImgData={setImgData} />
+      <ProductContentBottom imgData={imgData} setImgData={setImgData} />
     </P.ProductContentContainer>
   );
 }
--- src/pages/Detail/CommonDetail/ProductDetail.jsx
+++ src/pages/Detail/CommonDetail/ProductDetail.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductContentContainer from './ProductContentContainer';
 import * as P from './ProductDetailStyle';
 import ProductImgBox from './ProductImgBox';
 
 export default function ProductDetail() {
-  console.log(detailContent.price);
+  const [imgData, setImgData] = useState();
+  useEffect(() => {
+    fetch('http://3.38.247.226:3000/products/1', {
+      headers: {
+        Authorization:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY3NTcwNjQ5OH0.pu1WqqhWifWjC4D4Q_CSqQ2vDCJbAISzR7cnWXmNe5g',
+      },
+    })
+      .then(res => res.json())
+      .then(data => setImgData(data));
+  }, []);
+  // console.log(imgData);
+
   return (
     <P.ProductContainer>
-      <ProductImgBox />
-      <ProductContentContainer detailContent={detailContent} />
+      {imgData && (
+        <>
+          <ProductImgBox imgData={imgData} setImgData={setImgData} />
+          <ProductContentContainer imgData={imgData} setImgData={setImgData} />
+        </>
+      )}
     </P.ProductContainer>
   );
 }
-const detailContent = {
-  id: 1,
-  name: 'cat Tower',
-  price: '20000',
-};
